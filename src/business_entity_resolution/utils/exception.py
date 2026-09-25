650fd12a7673edd98ed35e9074dc9b5acc29f217
jatinvimal1506 <jatinkvimal@gmail.com>
2026-09-26 04:10:52 +0530

    Added custom Exception Handling

---
+++ src/business_entity_resolution/utils/exception.py
@@ -0,0 +1,27 @@
+import sys
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = self.get_detailed_error_message(
+            error_message, error_detail
+        )
+
+    @staticmethod
+    def get_detailed_error_message(error_message, error_detail: sys):
+        _, _, exc_tb = error_detail.exc_info()
+
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+        detailed_error_message = (
+            f"Error occurred in script: {file_name} "
+            f"at line number: {line_number} "
+            f"error message: {error_message}"
+        )
+
+        return detailed_error_message
+
+    def __str__(self):
+        return self.error_message
